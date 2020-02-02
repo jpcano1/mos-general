507fd981bc058ffe3069fe7558c24c2d5f232e21
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2020-02-01 20:53:38 -0500

    Modelaje del 4 punto acabado, pero no funciona

---
+++ guias/ejercicio_4.py
@@ -0,0 +1,71 @@
+from __future__ import division
+from pyomo.environ import *
+
+from pyomo.opt import SolverFactory
+
+Model = ConcreteModel()
+
+''' Modelado del problema número 4 '''
+distBetweenCities = {
+    (1, 1): 0,
+    (1, 2): 10,
+    (1, 3): 20,
+    (1, 4): 30,
+    (1, 5): 30,
+    (1, 6): 20,
+
+    (2, 1): 10,
+    (2, 2): 0,
+    (2, 3): 25,
+    (2, 4): 35,
+    (2, 5): 20,
+    (2, 6): 10,
+
+    (3, 1): 20,
+    (3, 2): 25,
+    (3, 3): 0,
+    (3, 4): 15,
+    (3, 5): 30,
+    (3, 6): 20,
+
+    (4, 1): 30,
+    (4, 2): 35,
+    (4, 3): 15,
+    (4, 4): 0,
+    (4, 5): 15,
+    (4, 6): 25,
+
+    (5, 1): 30,
+    (5, 2): 20,
+    (5, 3): 30,
+    (5, 4): 15,
+    (5, 5): 0,
+    (5, 6): 14,
+
+    (6, 1): 20,
+    (6, 2): 10,
+    (6, 3): 20,
+    (6, 4): 25,
+    (6, 5): 14,
+    (6, 6): 0,
+}
+
+cities = [1, 2, 3, 4, 5, 6]
+
+Model.cities = Var(cities, within=Binary)
+
+def objective(model):
+    operation = sum(model.cities[i] * distBetweenCities[i, j] for i in cities for j in cities if i != j)
+    return operation
+
+Model.obj = Objective(rule=objective, sense=minimize)
+
+def least_time(model, i, j):
+    dist = model.cities[i] * distBetweenCities[i, j]
+    return dist <= 15 if i != j else Constraint.NoConstraint
+
+Model.con_1 = Constraint(cities, cities, rule=least_time)
+
+SolverFactory('glpk').solve(Model)
+
+Model.display()
