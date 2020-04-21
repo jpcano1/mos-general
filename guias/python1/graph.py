bc5f88802c4288d7ffa6b1914fa907c816ab2a39
Juan Pablo <jp.cano@uniandes.edu.co>
2020-04-21 18:11:56 -0500

    Taller terminado

--- guias/python1/graph.py
+++ guias/python1/graph.py
@@ -1,6 +1,13 @@
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+
+# Functions
+def distance(x1, y1, x2, y2):
+    return np.sqrt((x2 - x1) ** 2 + ( y2 - y1) ** 2)
 
 # Plots
 figure = plt.figure(figsize=(5, 5))
@@ -8,21 +15,17 @@ ax = figure.add_subplot(1, 1, 1)
 ax.set(xlim=(-5, 105), ylim=(-5, 105))
 ax.grid(1)
 
-# Functions
-def distance(x1, y1, x2, y2):
-    return np.sqrt((x2 - x1) ** 2 + ( y2 - y1) ** 2)
-
-points = pd.DataFrame(data=np.random.uniform(0, 100, size=(100, 2)), columns=["x", "y"])
+points = pd.DataFrame(data=np.random.uniform(0, 100, size=(100, 2)), columns=["x", "y"], index=np.arange(1, 101))
 ax.plot(points["x"], points["y"], "ko")
 
 edges = []
 
 for i in range(len(points)):
-    ax.text(x=points["x"][i] - 0.1, y=points["y"][i] - 0.1, s=(i+1))
+    ax.text(x=points["x"].iloc[i] - 0.1, y=points["y"].iloc[i] - 0.1, s=(i+1))
 
     for j in range(i+1, len(points)):
-        first = points.loc[i]
-        second = points.loc[j]
+        first = points.iloc[i]
+        second = points.iloc[j]
         x1, y1, x2, y2 = first["x"], first["y"], second["x"], second["y"]
         dist = distance(x1, y1, x2, y2)
         if 0 < dist <= 15:
@@ -32,5 +35,64 @@ edges = pd.DataFrame(edges, columns=["Inicial", "Final"])
 
 for i in range(len(edges)):
     ax.plot(edges["Inicial"][i], edges["Final"][i], "k--")
+
+# Hasta aquí va  la grafica de nodos y arcos
+edges = {}
+for i in range(1, len(points) + 1):
+    for j in range(1, len(points) + 1):
+        first = points.loc[i]
+        second = points.loc[j]
+        x1, y1, x2, y2 = first["x"], first["y"], second["x"], second["y"]
+        dist = distance(x1, y1, x2, y2)
+        if 0 < dist <= 15:
+            edges[i, j] = dist
+        else:
+            edges[i, j] = 999
+
+p = RangeSet(1, 100)
+
+sourceNode = int(input("Ingrese el nodo de salida: "))
+destinationNode = int(input("Ingrese el nodo de llegada: "))
+
+Model =ConcreteModel()
+
+Model.x = Var(p, p, domain=Binary)
+Model.obj = Objective(expr=sum(Model.x[i, j] * edges[i, j] for i in p for j in p), sense=minimize)
+
+def source_rule(model, i):
+    if i == sourceNode:
+        return sum(model.x[i, j] for j in p) == 1
+    else:
+        return Constraint.Skip
+
+Model.source = Constraint(p, rule=source_rule)
+
+def destination_rule(model, j):
+    if j == destinationNode:
+        return sum(model.x[i, j] for i in p) == 1
+    else:
+        return Constraint.Skip
+
+Model.destination = Constraint(p, rule=destination_rule)
+
+def intermediate_rule(model, i):
+    if i != sourceNode and i != destinationNode:
+        return sum(model.x[i, j] for j in p) - sum(model.x[j, i] for j in p) == 0
+    else:
+        return Constraint.Skip
+
+Model.intermediate = Constraint(p, rule=intermediate_rule)
+
+def not_repeat_edge_rule(model, i, j):
+    return model.x[i, j] + model.x[j, i] <= 1
+
+Model.notRepeatEdge = Constraint(p, p, rule=not_repeat_edge_rule)
+
+SolverFactory('glpk').solve(Model)
+
+for i in range(1, len(points) + 1):
+    for j in range(1, len(points) + 1):
+        if Model.x[i, j].value == 1:
+            ax.plot([points.loc[i, "x"], points.loc[j, "x"]], [points.loc[i, "y"], points.loc[j, "y"]], "r--")
 
 plt.show()
