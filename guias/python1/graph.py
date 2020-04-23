b5c67febe46aab1fc74c19d3dce1cebd597bf858
Juan Pablo <jp.cano@uniandes.edu.co>
2020-04-23 18:27:48 -0500

    Grafo casi terminado

--- guias/python1/graph.py
+++ guias/python1/graph.py
@@ -1,9 +1,6 @@
-from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from pyomo.environ import *
-from pyomo.opt import SolverFactory
 
 """
 @requires pandas, numpy
@@ -11,6 +8,74 @@ from pyomo.opt import SolverFactory
 # Functions
 def distance(x1, y1, x2, y2):
     return np.sqrt((x2 - x1) ** 2 + ( y2 - y1) ** 2)
+
+def dijkstra(graph, start, goal):
+    """
+    Crea el camino de menor costo entre un
+    punto de inicio y otro de llegada
+    @param graph: el espacio de busqueda
+    @param start: nodo de inicio
+    @param goal: nodo de llegada
+    @return: el camino de menor costo
+    """
+    shortest_distance = {}
+    predecessor = {}
+    unseenNodes = graph.copy()
+    infinity = 9999999
+    path = []
+    for node in unseenNodes:
+        shortest_distance[node] = infinity
+    shortest_distance[start] = 0
+
+    while unseenNodes:
+        minNode = None
+        for node in unseenNodes:
+            if minNode is None:
+                minNode = node
+            elif shortest_distance[node] < shortest_distance[minNode]:
+                minNode = node
+
+        for childNode, weight in graph[minNode].items():
+            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[minNode]
+                predecessor[childNode] = minNode
+        unseenNodes.pop(minNode)
+
+    currentNode = goal
+    while currentNode != start:
+        try:
+            path.insert(0, currentNode)
+            currentNode = predecessor[currentNode]
+        except KeyError:
+            print('Path not reachable')
+            break
+
+    path.insert(0, start)
+    if shortest_distance[goal] != infinity:
+        return path
+    else:
+        return None
+
+def to_graph(p, e):
+    """
+    Crea el grafo a partir de puntos y arcos
+    @param p: los puntos del grafo
+    @param e: los arcos del grafo
+    @return: El grafo en forma de diccionario
+    """
+    frame = pd.DataFrame(e.loc[:, ["i", "j", "Weight"]])
+    frame = frame.set_index("i")
+    g = {}
+    for point in p.index:
+        neighbors = {}
+        try:
+            for neighbor, weight in frame.loc[point].values:
+                neighbors[int(neighbor)] = weight
+            g[point] = neighbors
+        except:
+            g[point] = neighbors
+            continue
+    return g
 
 # Styles
 plt.style.use("dark_background")
@@ -29,9 +94,9 @@ n_shades = 10
 diff_linewidth = 1.05
 alpha_value = 0.3 / n_shades
 
-points = pd.DataFrame(data=np.random.uniform(0, 100, size=(100, 2)),
-                      columns=["x", "y"], index=np.arange(1, 101))
-# points = pd.read_excel("points.xlsx", index_col=0)
+# points = pd.DataFrame(data=np.random.uniform(0, 100, size=(100, 2)),
+#                       columns=["x", "y"], index=np.arange(1, 101))
+points = pd.read_excel("points.xlsx", index_col=0)
 ax.plot(points["x"], points["y"], ".", color="#00fdff")
 
 edges = []
@@ -40,80 +105,23 @@ for i in range(len(points)):
     ax.text(x=points["x"].iloc[i] + 1, y=points["y"].iloc[i],
             s=(i+1), fontsize=8, weight='bold', color="#00fdff")
 
-    for j in range(i+1, len(points)):
+    for j in range(len(points)):
         first = points.iloc[i]
         second = points.iloc[j]
         x1, y1, x2, y2 = first["x"], first["y"], second["x"], second["y"]
         dist = distance(x1, y1, x2, y2)
         if 0 < dist <= 14:
-            edges.append([(x1, x2), (y1, y2)])
+            edges.append([(x1, x2), (y1, y2), i+1, j+1, dist])
 
-edges = pd.DataFrame(edges, columns=["Inicial", "Final"])
+edges = pd.DataFrame(edges, columns=["Inicial", "Final", "i", "j", "Weight"])
 
 for i in range(len(edges)):
     ax.plot(edges["Inicial"][i], edges["Final"][i],
             "--", color="#00fdff")
     ax.plot(edges["Inicial"][i], edges["Final"][i], "-",
-            color="#00fdff", alpha=0.1, linewidth=4.5)
+            color="#00fdff", alpha=0.1, linewidth=3.5)
+graph = to_graph(points, edges)
+path = dijkstra(graph, 22, 44)
 
-# Hasta aquí va la grafica de nodos y arcos
-edges = {}
-for i in range(1, len(points) + 1):
-    for j in range(1, len(points) + 1):
-        first = points.loc[i]
-        second = points.loc[j]
-        x1, y1, x2, y2 = first["x"], first["y"], second["x"], second["y"]
-        dist = distance(x1, y1, x2, y2)
-        if 0 < dist <= 14:
-            edges[i, j] = dist
-        else:
-            edges[i, j] = 9999
-
-p = RangeSet(1, 100)
-
-sourceNode = int(input("Ingrese el nodo de salida: "))
-destinationNode = int(input("Ingrese el nodo de llegada: "))
-
-Model =ConcreteModel()
-
-Model.x = Var(p, p, domain=Binary)
-Model.obj = Objective(expr=sum(Model.x[i, j] * edges[i, j] for i in p for j in p), sense=minimize)
-
-def source_rule(model, i):
-    if i == sourceNode:
-        return sum(model.x[i, j] for j in p) + sum(model.x[j, i] for j in p) == 1
-    else:
-        return Constraint.Skip
-
-Model.source = Constraint(p, rule=source_rule)
-
-def destination_rule(model, j):
-    if j == destinationNode:
-        return sum(model.x[i, j] for i in p) + sum(model.x[j, i] for i in p) == 1
-    else:
-        return Constraint.Skip
-
-Model.destination = Constraint(p, rule=destination_rule)
-
-def intermediate_rule(model, i):
-    if i != sourceNode and i != destinationNode:
-        return sum(model.x[i, j] for j in p) - sum(model.x[j, i] for j in p) == 0
-    else:
-        return Constraint.Skip
-
-Model.intermediate = Constraint(p, rule=intermediate_rule)
-
-def not_repeat_edge_rule(model, i, j):
-    return model.x[i, j] + model.x[j, i] <= 1
-
-Model.notRepeatEdge = Constraint(p, p, rule=not_repeat_edge_rule)
-
-SolverFactory('glpk').solve(Model)
-
-for i in range(1, len(points) + 1):
-    for j in range(1, len(points) + 1):
-        if Model.x[i, j] == 1 and edges[i, j] != 9999:
-            ax.plot([points.loc[i, "x"], points.loc[j, "x"]],
-                    [points.loc[i, "y"], points.loc[j, "y"]],
-                    "s-", color="#08ff08")
-plt.show()
+print(str(path))
+# plt.show()
